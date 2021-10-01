98dd8d3109241d425cf5d778afb7538b02ec1efa
Katherine <chiakyu@gmail.com>
2021-10-01 13:48:01 -0700

    add partial indexes to optimize queries

--- qaQueries.sql
+++ qaQueries.sql
@@ -12,14 +12,26 @@ SELECT * FROM photos;
 
 -- CREATE Results Table joining questions, answers and photos
 -- Should appear in order of helpfulness (and maybe date?)
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT *
+  FROM questions q
+  INNER JOIN answers a ON q.id = a.question_id
+  INNER JOIN photos p ON a.id = p.answer_id
+  WHERE q.reported = false AND a.reported = false AND product_id = 50000
+  ORDER BY q.product_id ASC, q.helpful DESC, a.helpful DESC
+
+  LIMIT 10;
+
+--TRY BELOW WITHOUT ORDER BY - ASSUMING INDEX WILL SORT
 EXPLAIN ANALYZE
 SELECT *
   FROM questions q
   INNER JOIN answers a ON q.id = a.question_id
   INNER JOIN photos p ON a.id = p.answer_id
   WHERE q.reported = false AND a.reported = false
-  ORDER BY q.product_id ASC, q.helpful DESC, a.helpful DESC;
-  -- LIMIT 20
+  ORDER BY q.product_id ASC
+  LIMIT 10;
+
 
 -- Results table for just answers (would be faster than the question query above)
 EXPLAIN ANALYZE
@@ -93,14 +105,14 @@ INSERT INTO questions (body, asker_name, asker_email, product_id)
 SELECT setval('answers_id_seq', (SELECT MAX (id) FROM answers)+1);
 
 -- need to reset id prior to entering new records
-EXPLAIN ANALYZE
+EXPLAIN (ANALYZE, BUFFERS)
 INSERT INTO answers (body, answerer_name, answerer_email, question_id)
   VALUES ('This is the only answer', 'testing123', 'test@gmail.com', 2);
 
 
 SELECT setval('photos_id_seq', (SELECT MAX (id) FROM photos)+1);
 
-EXPLAIN ANALYZE
+EXPLAIN (ANALYZE, BUFFERS)
 INSERT INTO photos (url, answer_id)
   VALUES ('testurl.com', 2);
 
@@ -108,7 +120,7 @@ INSERT INTO photos (url, answer_id)
 -- PUT /qa/questions/:question_id/helpful
 -- Updates a question to show it was found helpful.
 -- Parameter: question_id
-EXPLAIN ANALYZE
+EXPLAIN (ANALYZE, BUFFERS)
 UPDATE questions
   SET helpful = helpful + 1
   WHERE id = 3518965;
@@ -137,7 +149,68 @@ UPDATE answers
 -- PUT /qa/answers/:answer_id/report
 -- Updates an answer to show it has been reported. Note, this action does not delete the answer, but the answer will not be returned in the above GET request.
 -- Parameter: answer_id
-EXPLAIN ANALYZE
+EXPLAIN (ANALYZE, BUFFERS)
 UPDATE answers
   SET reported = true
   WHERE id = 6879308;
+
+
+
+----
+--ADD / DROP INDEXES
+----
+-- This set did nothing to improve speed
+CREATE INDEX ON questions (product_id);
+CREATE INDEX ON questions (helpful);
+
+DROP INDEX questions_product_id_idx;
+DROP INDEX questions_helpful_idx;
+
+CREATE INDEX ON questions (product_id ASC, helpful DESC);
+DROP INDEX questions_product_id_helpful_idx;
+
+CREATE INDEX ON questions (helpful DESC);
+DROP INDEX questions_helpful_idx;
+
+
+-- Try creating index on answers
+CREATE INDEX ON answers (question_id);
+DROP INDEX answers_question_id_idx;
+
+CREATE INDEX ON photos (answer_id);
+
+CREATE INDEX ON questions (helpful DESC);
+CREATE INDEX ON answers (helpful DESC);
+
+CREATE INDEX ON questions (reported);
+CREATE INDEX ON answers (reported);
+
+DROP INDEX photos_answer_id_idx;
+DROP INDEX questions_helpful_idx;
+DROP INDEX answers_helpful_idx;
+DROP INDEX questions_reported_idx;
+DROP INDEX answers_reported_idx;
+DROP INDEX answers_question_id_idx;
+
+CREATE INDEX ON questions (product_id);
+DROP INDEX questions_product_id_idx;
+
+------- TRY COMBO INDEXS
+--CURRENT:
+CREATE INDEX ON questions (product_id);
+--ADD:
+CREATE INDEX ON questions (product_id ASC, helpful DESC);
+CREATE INDEX ON answers (question_id ASC, helpful DESC);
+CREATE INDEX ON photos (answer_id ASC);
+
+--RESET INDEXES:
+DROP INDEX questions_product_id_helpful_idx;
+DROP INDEX answers_question_id_helpful_idx;
+DROP INDEX photos_answer_id_idx;
+
+
+
+------ TRY PARTIAL INDEXES
+CREATE INDEX ON questions (product_id ASC, helpful DESC) WHERE reported=false;
+CREATE INDEX ON answers (question_id ASC, helpful DESC) WHERE reported=false;
+CREATE INDEX ON photos (answer_id ASC);
