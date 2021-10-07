ae5a705694c685dc9ee433ec18fb1b6a1bffc351
Katherine <chiakyu@gmail.com>
2021-10-07 12:42:25 -0700

    update remote tests

--- tests/remoteStressTest.js
+++ tests/remoteStressTest.js
@@ -8,8 +8,8 @@ export let options = {
     { duration: '5s', target: 5 }, // below normal load (1/s)
     { duration: '5s', target: 50 }, // (10/s)
     { duration: '5s', target: 500 }, // normal load (50/s)
-    { duration: '5s', target: 1000 }, // around the breaking point (200/s)
-    { duration: '5s', target: 500 }, // beyond the breaking point
+    // { duration: '5s', target: 1000 }, // around the breaking point (200/s)
+    // { duration: '5s', target: 500 }, // beyond the breaking point
     { duration: '5s', target: 0 }, // scale down. Recovery stage.
   ],
   thresholds: {
@@ -22,40 +22,32 @@ export let options = {
 };
 
 
+
 export default function () {
   const SLEEP_DURATION = 1;
 
-  // 1,000,011 product ids
-  // 3,543,838 question ids
-  // 6,904,118 answer ids
-  const product_idMax = 1000000; // started with 100000
-  const product_idMin = Math.floor(product_idMax * .9); // test last 10% of data
-  const product_id = Math.round((Math.random() * (product_idMax-product_idMin)) + product_idMin);
+  const randomizer = (max, min) => {
+    if (min) {
+      return Math.round((Math.random() * (max-min)) + min);
+    }
+    let calcMin = Math.floor(max * .9)
+    return Math.round((Math.random() * (max-calcMin)) + calcMin);
+  }
 
-  const question_idMax = 3500000; // started with 100000
-  const question_idMin = Math.floor(question_idMax * .9); // test last 10% of data
-  const question_id = Math.round((Math.random() * (question_idMax-question_idMin)) + question_idMin);
-
-  const answer_idMax = 6900000; // started with 100000
-  const answer_idMin = Math.floor(answer_idMax * .9);
-  const answer_id = Math.round((Math.random() * (answer_idMax-answer_idMin)) + answer_idMin);
-
-  const countMax = 100;
-  const countMin = 5;
-  const count = Math.round((Math.random() * (countMax-countMin)) + countMin);
-
-  const pageMax = 5;
-  const pageMin = 1;
-  const page = Math.round((Math.random() * (pageMax-pageMin)) + pageMin);
+  const product_id = randomizer(1000000) // total 1,000,011 product ids
+  const question_id = randomizer(3500000) // total 3,543,838 question ids
+  const answer_id = randomizer(6900000) // total 6,904,118 answer ids
+  const count = randomizer(100, 5)
+  const page = randomizer(5, 1)
 
   const questionData = {
-    body: 'Testing: Will this work?',
+    body: 'Oct 7 Remote Testing: Will this work?',
     name: 'Cat Test',
     email: 'ilovetesting@yahoo.com',
     product_id: product_id
   }
   const answerData = {
-    body: 'Testing: This is the final answer. You can do it!',
+    body: 'Oct 7 Remote Testing: This is the final answer. You can do it!',
     name: 'Cat Test',
     email: 'ilovetesting@yahoo.com'
   }
